a25f72526e4b530f75d8d44b1f1c986f63c626af
silvia <silvia@maykinmedia.nl>
2021-09-08 17:51:45 +0200

    :construction: Parameter to specify attr consuming service index

--- src/onelogin/saml2/auth.py
+++ src/onelogin/saml2/auth.py
@@ -465,10 +465,10 @@ class OneLogin_Saml2_Auth(object):
         return self._last_response_in_response_to
 
     def _create_authn_request(
-        self, force_authn=False, is_passive=False, set_nameid_policy=True, name_id_value_req=None
+        self, force_authn=False, is_passive=False, set_nameid_policy=True, name_id_value_req=None, attr_consuming_service_index=None
     ):
         authn_request = OneLogin_Saml2_Authn_Request(
-            self._settings, force_authn, is_passive, set_nameid_policy, name_id_value_req
+            self._settings, force_authn, is_passive, set_nameid_policy, name_id_value_req, attr_consuming_service_index
         )
         self._last_request = authn_request.get_xml()
         self._last_request_id = authn_request.get_id()
@@ -503,7 +503,7 @@ class OneLogin_Saml2_Auth(object):
 
         return url, parameters
 
-    def login(self, return_to=None, force_authn=False, is_passive=False, set_nameid_policy=True, name_id_value_req=None):
+    def login(self, return_to=None, force_authn=False, is_passive=False, set_nameid_policy=True, name_id_value_req=None, attr_consuming_service_index=None):
         """
         Initiates the SSO process.
 
@@ -527,7 +527,8 @@ class OneLogin_Saml2_Auth(object):
         """
         authn_request = self._create_authn_request(
             force_authn=force_authn, is_passive=is_passive,
-            set_nameid_policy=set_nameid_policy, name_id_value_req=name_id_value_req
+            set_nameid_policy=set_nameid_policy, name_id_value_req=name_id_value_req,
+            attr_consuming_service_index=attr_consuming_service_index
         )
         self._last_request = authn_request.get_xml()
         self._last_request_id = authn_request.get_id()
