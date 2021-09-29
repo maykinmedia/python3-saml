7e24119f7171b0e89c893032178fdd2db7bce4b9
silvia <silvia@maykinmedia.nl>
2021-09-29 17:42:30 +0200

    :heavy_plus_sign: pyOpenSSL

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         'xmlsec>=1.3.9',
         'defusedxml>=0.5.0',
         'requests>=2.24.0',
+        'pyOpenSSL>=19.1.0',
     ],
     dependency_links=['http://github.com/mehcode/python-xmlsec/tarball/master'],
     extras_require={
