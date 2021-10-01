18ebade71646b8e0f8c14448db92514255cef7e7
silvia <silvia@maykinmedia.nl>
2021-10-01 16:43:19 +0200

    :construction: Replace givenName with telephoneNumber for contactPerson

--- src/onelogin/saml2/metadata.py
+++ src/onelogin/saml2/metadata.py
@@ -113,7 +113,7 @@ class OneLogin_Saml2_Metadata(object):
                 contact = OneLogin_Saml2_Templates.MD_CONTACT_PERSON % \
                     {
                         'type': ctype,
-                        'name': info['givenName'],
+                        'telephone': info['telephoneNumber'],
                         'email': info['emailAddress'],
                     }
                 contacts_info.append(contact)
